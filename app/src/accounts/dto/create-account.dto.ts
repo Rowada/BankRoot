5d0bdaed3f7194dbd330f648a66b97cc9f7fe7cc
Rowada <96113041+Rowada@users.noreply.github.com>
2023-02-05 16:23:03 +0100

    feat: add create and update feature for account

---
+++ app/src/accounts/dto/create-account.dto.ts
@@ -0,0 +1,11 @@
+import { IsNotEmpty, IsNumber } from 'class-validator';
+
+export class CreateAccountDto {
+  @IsNumber()
+  id_client: number;
+
+  @IsNumber()
+  account_id: number;
+  @IsNumber()
+  admin_id: number;
+}
--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ClientsModule } from './clients/clients.module';
+import { AccountsModule } from './accounts/accounts.module';
 
 @Module({
-  imports: [PrismaModule, ClientsModule],
+  imports: [PrismaModule, ClientsModule, AccountsModule],
   controllers: [AppController],
   providers: [AppService],
 })
